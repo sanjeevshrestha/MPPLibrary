e6cb1eb161195540b73bbe71a3709f378a0f7f72
Sanjeev Shrestha <sanjeevshrestha2004@gmail.com>
2015-11-30 11:55:39 -0600

    Project Stub

---
+++ src/mpplibrary/application/controllers/UserController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mpplibrary.application.controllers;
+
+/**
+ *
+ * @author 984970
+ */
+public class UserController {
+    
+}
