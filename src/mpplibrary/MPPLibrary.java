9b8c88a7a8ac333d837ae5af30b3fa57d5d969c4
Sanjeev Shrestha <sanjeevshrestha2004@gmail.com>
2015-12-01 10:51:53 -0600

    Database

--- src/mpplibrary/MPPLibrary.java
+++ src/mpplibrary/MPPLibrary.java
@@ -12,30 +12,31 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import mpplibrary.database.Database;
 
 /**
  *
  * @author 984970
  */
 public class MPPLibrary extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
         });
-        
+
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        
+
         Scene scene = new Scene(root, 300, 250);
-        
+
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -45,7 +46,9 @@ public class MPPLibrary extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         launch(args);
+
     }
-    
+
 }
