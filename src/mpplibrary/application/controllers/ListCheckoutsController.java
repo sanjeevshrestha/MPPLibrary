71429ebf91b7aa39b6c0a4265a7085527e1b248e
Sanjeev Shrestha <Sanjeevshrestha2004@gmail.com>
2015-12-06 17:40:36 -0600

    Fixes in book

--- src/mpplibrary/application/controllers/ListCheckoutsController.java
+++ src/mpplibrary/application/controllers/ListCheckoutsController.java
@@ -14,6 +14,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
@@ -51,6 +52,9 @@ public class ListCheckoutsController {
 
     @FXML
     AnchorPane pnCheckoutRecord;
+    
+    @FXML
+    Button btnCheckin,btnCalculateFine;
 
     private ObservableList<CheckoutRecord> checkoutList;
 
